da40a2c7ab8203ae784569232840254f305be372
Lennard <lennard_musch@outlook.de>
2025-09-14 15:40:03 +0200

    Wrap AES key in initializer list

--- src/crypto_storage.cpp
+++ src/crypto_storage.cpp
@@ -4,7 +4,7 @@
 #include <mbedtls/aes.h>
 
 const char* uid_file = "/uids.dat";
-const uint8_t aes_key[16] = AES_KEY; // siehe config.h
+const uint8_t aes_key[16] = { AES_KEY }; // siehe config.h
 
 void aes_encrypt(const uint8_t* plain, uint8_t* encrypted) {
     mbedtls_aes_context ctx;
@@ -22,4 +22,39 @@ void aes_decrypt(const uint8_t* encrypted, uint8_t* plain) {
     mbedtls_aes_free(&ctx);
 }
 
-// ... Rest wie bereits geliefert (init und checkUID)
+void CryptoStorage::init() {
+    if (!LittleFS.exists(uid_file)) {
+        File f = LittleFS.open(uid_file, "w");
+        if (f) {
+            f.close();
+        }
+    }
+}
+
+int CryptoStorage::checkUID(const String &uid) {
+    File f = LittleFS.open(uid_file, "r");
+    if (!f) {
+        return 0;
+    }
+
+    uint8_t encrypted[UID_ENTRY_SIZE];
+    uint8_t plain[UID_ENTRY_SIZE + 1];
+
+    while (f.read(encrypted, UID_ENTRY_SIZE) == UID_ENTRY_SIZE) {
+        aes_decrypt(encrypted, plain);
+        plain[UID_ENTRY_SIZE] = '\0';
+        String entry(reinterpret_cast<char*>(plain));
+        int sep = entry.indexOf(':');
+        if (sep > 0) {
+            String storedUid = entry.substring(0, sep);
+            int perm = entry.substring(sep + 1).toInt();
+            if (storedUid == uid) {
+                f.close();
+                return perm;
+            }
+        }
+    }
+
+    f.close();
+    return 0;
+}
